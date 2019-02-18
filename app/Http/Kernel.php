82c194da47bed210dd946c665ac054685772bfa0
nowhelloworld <phpfriend_xu@163.com>
2019-02-18 13:46:49 +0800

    mail

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class, // 确保在 StartSession 后面
         ],
 
         'api' => [
