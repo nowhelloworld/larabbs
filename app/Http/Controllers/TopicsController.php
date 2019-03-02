e45d52d4bd0ba26b1eabb554eb40aa3a0c7dbcc8
nowhelloworld <phpfriend_xu@163.com>
2019-03-02 18:08:51 +0800

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,8 +16,11 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
-		return view('topics.index', compact('topics'));
+        $topics = Topic::with('user', 'category')->paginate(30);
+        return view('topics.index', compact('topics'));
+
+		// $topics = Topic::paginate();
+		// return view('topics.index', compact('topics'));
 	}
 
     public function show(Topic $topic)
