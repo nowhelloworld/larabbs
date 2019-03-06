09c48e9cb8f6232673e55b616fa1522f660090ff
nowhelloworld <phpfriend_xu@163.com>
2019-03-06 18:17:52 +0800

    翻译 URL 标示

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,9 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+		// \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        // \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
         \Carbon\Carbon::setLocale('zh');
     }
