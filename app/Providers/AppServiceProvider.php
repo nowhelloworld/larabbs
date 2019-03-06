abe311851709a987211aabaeb46bdeb4f190eb25
nowhelloworld <phpfriend_xu@163.com>
2019-03-06 18:12:43 +0800

    翻译 URL 标示

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,8 +33,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (app()->isLocal()) {
-            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
-        }
+        // if (app()->isLocal()) {
+        //     $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        // }
     }
 }
