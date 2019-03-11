73dd8ce4a37c7028dba4635854b18a6344cb644a
nowhelloworld <phpfriend_xu@163.com>
2019-03-11 20:16:57 +0800

    verificationCode store

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -20,7 +20,7 @@ class VerificationCodesController extends Controller
 
             try {
                 $result = $easySms->send($phone, [
-                    'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                    'content'  =>  "【Lbbs社区】您的验证码是{$code}。"
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('yunpian')->getMessage();
