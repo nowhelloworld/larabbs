22f686f6d0fe9d3783ebbb7dfc5878d63773f675
nowhelloworld <phpfriend_xu@163.com>
2019-03-14 18:04:05 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,16 @@
 use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
-$group = [
-    'middleware' => 'api.throttle',
-    'limit' => config('api.rate_limits.sign.limit'),
-    'expires' => config('api.rate_limits.sign.expires'),
-];
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
 
-    $api->group($group, function($api) {
+    $api->group([
+            'middleware' => 'api.throttle',
+            'limit' => config('api.rate_limits.sign.limit'),
+            'expires' => config('api.rate_limits.sign.expires'),
+        ], function($api) {
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
