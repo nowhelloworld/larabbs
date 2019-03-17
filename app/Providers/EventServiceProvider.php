10f4dd1e93340c0331dc6938cab8a343c936c204
nowhelloworld <phpfriend_xu@163.com>
2019-03-17 16:06:12 +0800

    add socialiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -15,11 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-        \Illuminate\Auth\Events\Verified::class => [
-            \App\Listeners\EmailVerified::class,
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
         ],
     ];
 
