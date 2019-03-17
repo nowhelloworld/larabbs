6788258b18e0db435832e2a1a68f93a3620b7c3d
nowhelloworld <phpfriend_xu@163.com>
2019-03-17 13:00:46 +0800

    code后面不要跟标点符号，云片会认为那也是code了

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -33,7 +33,7 @@ class VerificationCodesController extends Controller
 
             try {
                 $result = $easySms->send($phone, [
-                    'content'  =>  "【个人学习php用】您的验证码是{$code}。"
+                    'content'  =>  "【个人学习php用】您的验证码是{$code}"
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('yunpian')->getMessage();
