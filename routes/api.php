2a2d301661baa691a564bb9af137fd6f1b1a5e80
nowhelloworld <phpfriend_xu@163.com>
2019-03-19 13:30:16 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,9 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    // 'middleware' => ['serializer:array', 'bindings', 'change-locale']
+    'middleware' => ['serializer:array']
 ], function($api) {
+
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
@@ -57,6 +58,22 @@ $api->version('v1', [
             ->name('api.authorizations.destroy');
 
     });
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
+
 });
 
 
