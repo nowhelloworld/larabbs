75e00f11d3c262e1cc8ed038fa21d9852d058ad9
nowhelloworld <phpfriend_xu@163.com>
2019-03-20 18:41:56 +0800

    locale

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -95,6 +95,10 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         // 访问节流，类似于 『1 分钟只能请求 10 次』的需求，一般在 API 中使用
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        // 接口语言设置
+        'change-locale' => \App\Http\Middleware\ChangeLocale::class,
+
         // Laravel 自带的强制用户邮箱认证的中间件，为了更加贴近我们的逻辑，已被重写
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
