35ecb73bed3941b50138a9e2df69c249a27c4d2a
nowhelloworld <phpfriend_xu@163.com>
2019-03-22 17:55:38 +0800

    weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,9 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'phone', 'email', 'password', 'introduction', 'avatar', 'weixin_openid', 'weixin_unionid'
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid', 'registration_id',
+        'weixin_session_key', 'weapp_openid',
     ];
 
     /**
